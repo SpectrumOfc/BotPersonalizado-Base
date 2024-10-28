fdfae639595a5936da791f1e726f6666493a617d
𝐒𝐏𝐄𝐂𝐓𝐑𝐔𝐌 𝐎𝐅𝐈𝐂𝐈𝐀𝐋 <cuentaoficialspectrum@gmail.com>
2024-10-28 04:55:55 -1100

    Update main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -12,7 +12,7 @@ let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp; 𝐒𝐏𝐄𝐂𝐓𝐑𝐔�
 let vcard2 = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp; 𝐘𝐈𝐑𝐎 𝐁𝐎𝐓\nNICKNAME:𝐘𝐈𝐑𝐎 𝐁𝐎𝐓\nORG: 𝐘𝐈𝐑𝐎 𝐁𝐎𝐓\nTITLE:soft\nitem1.TEL;waid=5217911032956:+52 1 791 103 2956\nitem1.X-ABLabel:📞 WhatsApp Owner\nitem2.URL:https://github.com/SpectrumOfc\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET: cuentaoficialspectrum@gmail.com\nitem3.X-ABLabel:💌 Correo soporte\nitem4.ADR:;;🇲🇽 MÉXICO;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel: Localización 🫧\nBDAY;value=date: 09-12-2007\nEND:VCARD`
 let vcard3 = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp; 𝐎𝐖𝐍𝐄𝐑\nNICKNAME:𝐎𝐖𝐍𝐄𝐑\nORG: 𝐎𝐖𝐍𝐄𝐑\nTITLE:soft\nitem1.TEL;waid=5215643260308:+52 1 56 4326 0308\nitem1.X-ABLabel:📞 WhatsApp Owner\nitem2.URL:https://github.com/SpectrumOfc\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET: cuentaoficialspectrum@gmail.com\nitem3.X-ABLabel:💌 Correo soporte\nitem4.ADR:;;🇲🇽 MÉXICO;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel: Localización 🫧\nBDAY;value=date: 09-12-2007\nEND:VCARD`
 const tag_own = await conn.sendMessage(m.chat, { contacts: { displayName: packname, contacts: [{ vcard }] }}, { quoted: fkontak })
-let caption = `*👋 Hola ${edtr}, este es el contacto de mi creador 🐈‍⬛*`
+let caption = `*👋 Hola ${edtr}, este es el contacto de mi creador*`
     await conn.reply(m.chat, caption, tag_own, { mentions: conn.parseMention(caption) })
 
 }
